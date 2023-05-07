8cf502bb6f147e9706f5631aa5ccecd5404bc946
pankaj <pankajsajekar789@gmail.com>
2023-05-07 12:00:54 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './home/about/about.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { GallaryComponent } from './home/gallary/gallary.component';
+import { PortfolioComponent } from './home/portfolio/portfolio.component';
+import { HeroComponent } from './home/hero/hero.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    GallaryComponent,
+    PortfolioComponent,
+    HeroComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
